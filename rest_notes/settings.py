32a370ea916f3e624d9cfe38b0e31d595075b546
Igor <gormal98@gmail.com>
2021-07-20 16:19:46 +0300

    add_renderers_and_todo_app_created

--- rest_notes/settings.py
+++ rest_notes/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'userapp',
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,10 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'userapp.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
