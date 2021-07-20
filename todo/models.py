e02afbf92c9d76a1b062af1005eaea007fbbc4ff
Igor <gormal98@gmail.com>
2021-07-20 17:58:55 +0300

    created_models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from userapp.models import User
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=64)
+    repository_url = models.CharField(max_length=128, blank=True)
+    users = models.ManyToManyField(User)
+
+
+class Todo(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(blank=True)    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+
