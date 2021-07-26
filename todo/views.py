5d0a245bdc8ab3a99f9eb940dde4ef7855d8ee24
Igor <gormal98@gmail.com>
2021-07-26 12:50:48 +0300

    pagination_and_filtered_projects_by_part_title

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,28 @@
 from rest_framework.viewsets import ModelViewSet
 from todo.models import Project, Todo
 from todo.serializers import ProjectModelSerializer, TodoModelSerializer
+from rest_framework.pagination import LimitOffsetPagination
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination): # по 10 проектов
+   default_limit = 10
+
+class TodoLimitOffsetPagination(LimitOffsetPagination): # по 20 заметок
+   default_limit = 20
 
 
 class ProjectModelViewSet(ModelViewSet):
-   queryset = Project.objects.all()
    serializer_class = ProjectModelSerializer
+   pagination_class = ProjectLimitOffsetPagination
+
+   def get_queryset(self):
+      name_for_search = self.request.query_params.get('project_name', None)
+      if name_for_search:
+         return Project.objects.filter(title__icontains=name_for_search)
+      return Project.objects.all()
+
 
 class TodoModelViewSet(ModelViewSet):
    queryset = Todo.objects.all()
    serializer_class = TodoModelSerializer
+   pagination_class = TodoLimitOffsetPagination
