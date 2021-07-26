977930e4f7d5d973a45847d5e4be075a931a25e1
Igor <gormal98@gmail.com>
2021-07-26 13:06:27 +0300

    add_restrict_to_users_only_list_and_retrieve

--- userapp/views.py
+++ userapp/views.py
@@ -1,8 +1,9 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from userapp.models import User
 from userapp.serializers import UserModelSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+class UserModelViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
    queryset = User.objects.all()
    serializer_class = UserModelSerializer
