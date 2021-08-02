721cafeee37823e8723faf7de90fabbb229fbea1
Igor <gormal98@gmail.com>
2021-08-02 19:20:00 +0300

    some_fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
    }
 
    componentDidMount() {
-    axios.get('http://127.0.0.1:8000/api/users')
+    axios.get('http://127.0.0.1:8000/api/users/')
         .then(response => {
             const users = response.data.results
                 this.setState(
@@ -27,6 +27,7 @@ class App extends React.Component {
     render () {
         return (
             <div>
+                
                 <UserList users={this.state.users} />
             </div>
         )
