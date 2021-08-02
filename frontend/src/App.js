5040309230a861b3f833f508aed0c807e5d3bb3b
Igor <gormal98@gmail.com>
2021-08-02 15:33:49 +0300

    create_app_react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,22 @@
-let a = 5;
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+   constructor(props) {
+       super(props)
+       this.state = {
+           'authors': []
+       }
+   }
+
+   render () {
+       return (
+           <div>
+               MAIN
+           </div>
+       )
+   }
+}
+
+export default App;
