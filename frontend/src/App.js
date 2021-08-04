0844304393179d05273a38785bfe7e07987d1934
Igor <gormal98@gmail.com>
2021-08-04 21:24:27 +0300

    added_links_todos_and_users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import UserList from './components/Users.js'
+import React from 'react'
 import axios from 'axios'
+// import logo from './logo.svg';
+import './App.css'
+import UserList from './components/Users.js'
+import TodoList from './components/Todos.js'
+import {HashRouter, Route, Link, Switch} from 'react-router-dom'
+
+const NotFound404 = ({ location }) => {
+    return (
+      <div>
+          <h1>Страница по адресу '{location.pathname}' не найдена</h1>
+      </div>
+    )
+  }
+
 
 class App extends React.Component {
    constructor(props) {
        super(props)
        this.state = {
-           'users': []
+           'users': [],
+           'projects': [],
+           'todos':[]
        }
    }
 
@@ -22,17 +35,41 @@ class App extends React.Component {
                 }
             )
         }).catch(error => console.log(error))
-   }
 
-    render () {
+        axios.get('http://127.0.0.1:8000/api/todos/')
+        .then(response => {
+            const todos = response.data.results
+                this.setState(
+                {
+                    'todos': todos
+                }
+            )
+        }).catch(error => console.log(error))
+   }    
+
+    render() {
         return (
             <div>
-                
-                <UserList users={this.state.users} />
+              <HashRouter>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to='/'>Users</Link>
+                  </li>
+                  <li>
+                    <Link to='/todos'>Todos</Link>
+                  </li>
+                </ul>
+              </nav>
+                <Switch>
+                  <Route exact path='/' component={() => <UserList users={this.state.users} />}  />
+                  <Route exact path='/todos' component={() => <TodoList todos={this.state.todos} />} />
+                  <Route component={NotFound404} />
+                </Switch>
+              </HashRouter>
             </div>
         )
-    }
-
+      }
 }
 
 export default App;
