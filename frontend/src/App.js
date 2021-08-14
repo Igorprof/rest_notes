18e199b420e14d50b9349ba4f40950444aab81b8
Igor <gormal98@gmail.com>
2021-08-14 22:31:32 +0300

    get_token_method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import './App.css'
 import UserList from './components/Users.js'
 import TodoList from './components/Todos.js'
 import ProjectList from './components/Projects.js'
+import LoginForm from './components/LoginForm.js'
 import {HashRouter, Route, Link, Switch, Redirect} from 'react-router-dom'
 
 const NotFound404 = ({ location }) => {
@@ -58,6 +59,17 @@ class App extends React.Component {
         }).catch(error => console.log(error))
    }    
 
+   get_token(login, password) {
+    axios.post('http://127.0.0.1:8000/api-token-auth/', 
+      {
+        "username": login, 
+        "password": password
+      })
+        .then(response => {
+            console.log(response.data)
+        }).catch(error => console.log(error))
+   }
+
     render() {
         return (
             <div>
@@ -73,12 +85,16 @@ class App extends React.Component {
                   <li>
                     <Link to='/projects'>Projects</Link>
                   </li>
+                  <li>
+                    <Link to='/login'>Login</Link>
+                  </li>
                 </ul>
               </nav>
                 <Switch>
                   <Route exact path='/' component={() => <UserList users={this.state.users} />}  />
                   <Route exact path='/todos' component={() => <TodoList todos={this.state.todos} />} />
-                  <Route exact path='/projects' component={() => <ProjectList projects={this.state.projects} />} />              
+                  <Route exact path='/projects' component={() => <ProjectList projects={this.state.projects} />} />   
+                  <Route exact path='/login' component={() => <LoginForm get_token={(login, password) => this.get_token(login, password)} />} />              
                   <Redirect from='/users' to='/' />    
                   <Route component={NotFound404} />
                 </Switch>
